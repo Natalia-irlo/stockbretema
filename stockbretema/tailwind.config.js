737e9875c08dd025d48764b6e181cf697a7786e1
natalia <natiriarte5@gmail.com>
2023-03-09 12:42:53 +0100

    tailwind

---
+++ stockbretema/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge: [
+      './resources/**/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
+    ],
+    darkMode: false, // o 'media' o 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  
