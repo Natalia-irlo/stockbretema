ac014f64017e1a71f17c3879ebca8c9fce4200a6
natalia <natiriarte5@gmail.com>
2023-03-15 15:38:02 +0100

    crud

--- stockbretema/routes/web.php
+++ stockbretema/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::resource('products', ProductController::class);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+?>
